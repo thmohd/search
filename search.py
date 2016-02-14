0b7b4edbbced36007cf492d8b438d2a47a5e283f
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2016-02-13 20:03:46 -0500

    refactor method names

--- search.py
+++ search.py
@@ -13,7 +13,7 @@ def find_link(page, startpos):
 	url = page[start_qoute + 1 : end_qoute]
 	return url, link_start
 
-def find_all(page):
+def get_all_links(page):
 	endpos = 0
 	while True:
 	     url, endpos = find_link(page, endpos + 1)
@@ -24,5 +24,5 @@ def find_all(page):
 		
 
 
-find_all(get_page('http://www.github.com'))
-#find_all('shgshgfhjsdg<a href="facebook.com">link</a>fhjsdg<a href="youtube.com">link</a>shgfhjsdg<a href="google.com">link</a>')
+get_all_links(get_page('http://www.github.com'))
+#get_all_links('shgshgfhjsdg<a href="facebook.com">link</a>fhjsdg<a href="youtube.com">link</a>shgfhjsdg<a href="google.com">link</a>')
